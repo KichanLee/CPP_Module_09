98de283a92c9a07e0e4bd908efefd88fb128dd99
KichanLee <lkc0847@naver.com>
2024-02-22 14:06:44 +0900

    map input

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -5,7 +5,7 @@ int main() {
   std::string str = "Hello, World!";
 
   // Create a substring from position 7 of length 5
-  std::string sub = str.substr(7, 5);
+  std::string sub = str.substr(7, 1);
   std::cout << "Substring: " << sub << std::endl;  // Outputs: World
 
   // Create a substring from position 7 until the end of the string
